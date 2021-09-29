84d948e102405518219ca2b320e2769c8bbb888f
Luiza Izeti <li41346@ubt-uni.net>
2021-09-29 23:00:28 +0200

    li

--- src/Car/CarsList.js
+++ src/Car/CarsList.js
@@ -1,76 +1,81 @@
-import React, { useEffect, useState } from 'react';  
-import {Link} from 'react-router-dom';
-import axios from 'axios';  
-import { getDefaultNormalizer } from '@testing-library/dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { getDefaultNormalizer } from "@testing-library/dom";
 
 function CarsList() {
   const [business, setbussines] = useState();
   const [status, setStatus] = useState();
 
-  const  getData = () => {
-    axios.get('http://localhost:65424/Api/Car/CarsList')  
-    .then(response => {
-      setbussines(response.data.cars);  
-    })  
-    .catch((error) => {  
-      console.log(error);  
-    });
-  }
+  const getData = () => {
+    axios
+      .get("http://localhost:65424/Api/Car/CarsList")
+      .then((response) => {
+        setbussines(response.data.cars);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  const deleteCar = (id) => {  
-    axios.post(`http://localhost:65424/Api/Car/Delete/${id}`)  
-   .then(res => {
-     console.log('res', res);
-    if (res.data) {
-      getData();
-      setStatus(res.data);
-      setTimeout(() => {
-        setStatus(null);
-      }, 4000);
-      alert('Record deleted successfully!!');  
-    }  
-   })  
-   } 
+  const deleteCar = (id) => {
+    axios.post(`http://localhost:65424/Api/Car/Delete/${id}`).then((res) => {
+      console.log("res", res);
+      if (res.data) {
+        getData();
+        setStatus(res.data);
+        setTimeout(() => {
+          setStatus(null);
+        }, 4000);
+        alert("Record deleted successfully!!");
+      }
+    });
+  };
 
   useEffect(() => {
-   getData();
+    getData();
   }, []);
 
-      return (  
-        <div>  
-          <h4 align="center">Cars List</h4>
-          {status && <p className="color-red">Deleted successfully!</p>}
-          <table className="table table-striped" style={{ marginTop: 10 }}>  
-            <thead>  
-              <tr>  
-                <th>Name</th>  
-                <th>Brand</th>  
-                <th>Color</th>  
-                <th>ChassisNumber</th>  
-                <th colSpan="4">Action</th>  
-              </tr>  
-            </thead>  
-            <tbody>
-              {
-                business?.map((el) => (
-                  <tr key={el.id}>
-                    <td>{el.name}</td>
-                    <td>{el.brand}</td>
-                    <td>  
-                      <Link to={`/EditCar/${el.id}`} className="btn btn-success">Edit</Link>  
-                    </td>  
-                    <td>  
-                      <button type="button" onClick={() => deleteCar(el.id)} className="btn btn-danger">Delete</button>  
-                    </td> 
-                  </tr>
-                  
-                ))
-              }
-             {/* {tabRow()}    */}
-            </tbody>  
-          </table>  
-        </div>  
-    ); 
-  }
+  return (
+    <div className="containerr">
+      <h4 align="center">Cars List</h4>
+      {status && <p className="color-red">Deleted successfully!</p>}
+      <table className="table table-striped" style={{ marginTop: 10 }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Brand</th>
+            <th>Color</th>
+            <th>ChassisNumber</th>
+            <th colSpan="4">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {business?.map((el) => (
+            <tr key={el.id}>
+              <td>{el.name}</td>
+              <td>{el.brand}</td>
+              <td>
+                <Link to={`/EditCar/${el.id}`} className="btn btn-success">
+                  Edit
+                </Link>
+              </td>
+              <td>
+                <button
+                  type="button"
+                  onClick={() => deleteCar(el.id)}
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+          {/* {tabRow()}    */}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
-  export default CarsList;
+export default CarsList;
